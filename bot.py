a69ef768952567432da02de41c24bef5b8ad3f77
Sean McHugh <mchughseanp@gmail.com>
2024-01-26 22:02:03 -0600

    Changed user class to track start and current ranks rather than have user class and ranks class. Updated registration command and get information methods to reflect this.

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ from discord import app_commands
 from discord.ext import commands
 from constants import DISCORD_TOKEN
 import commands
+from pagination import Pagination
 
 # Sets up client/bot and command tree
 client = discord.Client(intents = discord.Intents.default())
@@ -19,18 +20,21 @@ async def on_ready():
         print("Synced 0 commands successfully.")
         exit()
 
+# Temporary test command to ensure bot is connected and working
 @tree.command(name="hello", description="Says hello to you")
 @app_commands.describe()
 async def hello(interaction: discord.Interaction):
     await interaction.response.send_message(f"Hello {interaction.user.mention}!")
 
+# Setup options for registration command
 rankChoices = []
-for rank in ["Diamond", "Emerald", "Platinum", "Gold", "Silver", "Bronze", "Iron"]:
+for rank in ["DIAMOND", "EMERALD", "PLATINUM", "GOLD", "SILVER", "BRONZE", "IRON"]:
     rankChoices.append(discord.app_commands.Choice(name = rank, value = rank))
 divisionChoices = []
 for division in ["I", "II", "III", "IV"]:
     divisionChoices.append(discord.app_commands.Choice(name = division, value = division))
 
+# Create registration command
 @tree.command(name = "register", description = "Connect your league and discord accounts")
 @app_commands.describe(username = "Riot Games name", tag = "Riot Games tag", rank = "Choose your starting rank",
                        division = "Choose your starting division", lp = "Choose your starting LP")
@@ -44,7 +48,6 @@ async def register(interaction: discord.Interaction, username: str, tag: str, ra
         await interaction.response.send_message(e)
 
 @tree.command(name = "unregister", description = "Unregister your account from this bot in this server.")
-@app_commands.describe()
 async def unregister(interaction: discord.Interaction):
     try:
         commands.unregister(interaction.user.id, interaction.guild.id)
@@ -52,4 +55,24 @@ async def unregister(interaction: discord.Interaction):
     except Exception as e:
         await interaction.response.send_message(e)
 
+L = 5    # elements per page
+
+@tree.command(name = "leaderboard", description = "Display the rank improvement leaderboard for this server")
+async def leaderboard(interaction: discord.Interaction):
+    async def get_page(page: int):
+        emb = discord.Embed(title = "Rank Improvement Leaderboard",
+                            description = "",
+                            color = discord.Color.from_str("#101539"))
+        emb.set_thumbnail(url = "https://i.imgur.com/0QKRQ5V.png")
+        
+        offset = (page - 1) * L
+        users = commands.getImprovementLeaderboard(interaction.guild.id)
+        for user in users[offset:offset+L]:
+            emb.description += f"{user}\n"
+        n = Pagination.compute_total_pages(len(users), L)
+        emb.set_footer(text=f"Page {page} from {n}")
+        return emb, n
+
+    await Pagination(interaction, get_page).navegate()
+    
 client.run(DISCORD_TOKEN)
--- users.py
+++ users.py
@@ -1,7 +1,12 @@
 from sqlalchemy import ForeignKey, String, Integer, CHAR, Column, Boolean
 import sqlalchemy.orm
+import requests
+import constants
 
 Base = sqlalchemy.orm.declarative_base()
+
+ranks = {"IRON": 0, "BRONZE": 400, "SILVER": 800, "GOLD": 1200, "PLATINUM": 1600, "EMERALD": 2000, "DIAMOND": 2400}
+divisions = {"I": 300, "II": 200, "III": 100, "IV": 0}
 
 class User(Base):
     __tablename__ = "users"
@@ -13,28 +18,81 @@ class User(Base):
     # Riot Games identifying information
     username = Column("username", String)
     tag = Column("tag", String)
-    puuid = Column("puuid", String)
-    summonerId = Column("summonerId", String)
-    accountId = Column("accountId", String)
+    puuID = Column("puuID", String)
+    summonerID = Column("summonerID", String)
+    accountID = Column("accountID", String)
 
-    # User rank information
-    rank = Column("rank", String)
-    division = Column("division", String)
-    lp = Column("lp", Integer)
-    value = Column("value", Integer)
-    currRank = Column("currRank", Boolean, primary_key = True)
+    # Start rank info
+    startRank = Column("startRank", String)
+    startDivision = Column("startDivision", String)
+    startLP = Column("startLP", Integer)
+    startValue = Column("startValue", Integer)
+    
+    currRank = Column("currRank", String)
+    currDivision = Column("currDivision", String)
+    currLP = Column("currLP", Integer)
+    currValue = Column("currValue", Integer)
 
-    def __init__(self, discordId, serverId, username, tag, puuid,
-                 summonerId, accountId, rank, division, lp, value, currRank):
+    valueChange = Column("valueChange", Integer)
+        
+    def __init__(self, discordId, serverId, username, tag, startRank, startDivision, startLP):
         self.discordId = discordId
         self.serverId = serverId
+        
         self.username = username
         self.tag = tag
-        self.puuid = puuid
-        self.summonerId = summonerId
-        self.accountId = accountId
-        self.rank = rank
-        self.division = division
-        self.lp = lp
-        self.value = value
-        self.currRank = currRank
+        self.puuID = getPuuID(username, tag)
+        self.summonerID = getSummonerID(self.puuID)
+        self.accountID = getAccountID(self.puuID)
+
+        self.startRank = startRank
+        self.startDivision = startDivision
+        self.startLP = startLP
+        self.startValue = getRankValue(startRank, startDivision, startLP)
+
+        self.currRank, self.currDivision, self.currLP = getCurrRank(self.summonerID)
+        self.currValue = getRankValue(self.currRank, self.currDivision, self.currLP)
+
+        self.valueChange = self.currValue - self.startValue
+
+    def __repr__(self):
+        if self.currValue > 0:
+            return f"{self.username} {self.currRank} {self.currDivision} {self.currLP} (+{self.valueChange} lp)"
+        else:
+            return f"**{self.username}** {self.currRank} {self.currDivision} {self.currLP} (-{self.valueChange} lp)"
+        
+def getPuuID(username, tag):
+    response = requests.get(f"https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{username}/{tag}?api_key={constants.RIOT_KEY}")
+    if response.status_code != 200:
+        raise Exception("Invalid Riot Games name and/or tag")
+    response = response.json()
+    return response["puuid"]
+
+def getSummonerID(puuid):
+    response = requests.get(f"https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/{puuid}?api_key={constants.RIOT_KEY}")
+    if response.status_code != 200:
+        raise Exception("Error getting account and summoner id from Riot Games")
+    response = response.json()
+    return response["id"]
+
+def getAccountID(puuid):
+    response = requests.get(f"https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/{puuid}?api_key={constants.RIOT_KEY}")
+    if response.status_code != 200:
+        raise Exception("Error getting account and summoner id from Riot Games")
+    response = response.json()
+    return response["accountId"]
+
+def getRankValue(rank, division, lp):
+    if rank not in ["Master", "Grandmaster", "Challenger"]:
+        return ranks[rank] + divisions[division] + lp
+    else:
+        return lp + 2800
+
+def getCurrRank(summonerID):
+    response = requests.get(f"https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/{summonerID}?api_key={constants.RIOT_KEY}")
+    if response.status_code != 200:
+        raise Exception("Error getting current rank from Riot Games")
+    if response == []:
+        raise Exception("Player is unranked")
+    response = response.json()
+    return response[0]["tier"], response[0]["rank"], response[0]["leaguePoints"]
